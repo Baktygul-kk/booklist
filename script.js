a75f4057a2e87b2bb926041f883404e3083e0a64
Baktygul Kozhoshova <87691605+Baktygul-kk@users.noreply.github.com>
2022-03-06 23:40:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,40 +6,41 @@ function Book (title, author, isbn){
 
 function UI() { }
 
-// UI.prototype.addBookList = function (book) {
-// const table = document.getElementById("list");
-// const tr = document.createElement("tr");
-// console.log(table.append(tr));
+UI.prototype.addBookList = function (book) {
+const list = document.getElementById("book-list");
+const row = document.createElement("tr");
 
-// };
+row.innerHTML=`
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td>${book.isbn}</td>
+<td><a href="#" class="delete" >X</a></td>`
+
+list.appendChild(row);
+
+};
 
 
-UI.prototype.clearFields = function () {};
+UI.prototype.clearFields = function () {
+ document.getElementById("book-title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value ="";
+};
 
 
-Book.prototype.toString = function bookToString() {
-    return ` ${this.title} is a ${this.author} ${this.isbn}`;
-  }
 
 document.getElementById("book-form").addEventListener("submit", (e) => {
-   e.preventDefault();
    const title = document.getElementById("book-title").value;
    const author = document.getElementById("author").value;
    const isbn = document.getElementById("isbn").value;
     
-   console.log(title);
    
    const book = new Book (title, author, isbn);
    const ui = new UI ();
-   //ui.addBookList(book);
+  ui.addBookList(book);
+   ui.clearFields();
 
-   const table = document.getElementById("list");
-   const tr = document.createElement("tr");
-   console.log(table.append(tr));
-  const m = Object.values(book);
-   tr.innerHTML=m;
 
-   console.log(book);
-
+   e.preventDefault();
 
 });
