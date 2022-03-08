2a83919ebede677eddc07d52f9e5063f8f0171ab
Baktygul Kozhoshova <87691605+Baktygul-kk@users.noreply.github.com>
2022-03-08 19:04:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,29 @@ UI.prototype.clearFields = function () {
   document.getElementById("isbn").value ="";
 };
 
+UI.prototype.deleteBook = function(target) {
+  if (target.className === "delete"){
+    target.parentElement.parentElement.remove();
+
+  }
+}
+
+UI.prototype.showAlert = function(msg, className){
+const div = document.createElement("div");
+div.className = `alert ${className}`;
+
+div.appendChild(document.createTextNode(msg));
+
+const container = document.querySelector(".container");
+const form = document.querySelector("#book-form");
+
+container.insertBefore(div, form);
+
+setTimeout(function () {
+  document.querySelector(".alert").remove();
+}, 3000);
+};
+
 
 
 document.getElementById("book-form").addEventListener("submit", (e) => {
@@ -37,10 +60,23 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
    
    const book = new Book (title, author, isbn);
    const ui = new UI ();
-  ui.addBookList(book);
-   ui.clearFields();
-
-
+   if (title === "" || author ==="" || isbn ===""){
+     ui.showAlert("Please fill in all fields!", "error");
+   } else {
+    ui.addBookList(book);
+    ui.showAlert("book added!", "success");
+    ui.clearFields();
+ 
+   }
+   
    e.preventDefault();
 
+});
+
+document.getElementById("book-list").addEventListener('click', (e) => {
+  const ui = new UI();
+  ui.deleteBook(e.target);
+  ui.showAlert("book deleted!", "success");
+
+  e.preventDefault();
 });
